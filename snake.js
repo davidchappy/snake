69c44eb25f9fcc1d96da227541083c57a907d1ed
David Chapman <davidchappy@gmail.com>
2016-11-19 16:52:30 -0300

    Refactor

--- snake.js
+++ snake.js
@@ -1,22 +1,3 @@
-// Helpers 
-function getCoordinates(gridSize, xLength) {
-  coords = [];
-  x = 0;
-  y = 0;
-  for(var i = 0; i < gridSize; i++) {
-    if (i === 0 || i % xLength === 0) {
-      x++;
-    }
-    if (i <= 39) {
-      y = i+1;
-    } else {
-      y = (i % xLength)+1;
-    }
-    coords[i] = [x, y];  
-  };
-  return coords;
-};
-
 // DOM elements
 var $grid = $('#grid');
 
@@ -27,16 +8,32 @@ var grid = {
   defaultSquare: " ",
   gridCoords: [],
   renderGrid: function() {
-    for(var i = 0; i < this.gridSize; i++) {
+    for(let i = 0; i < this.gridSize; i++) {
        $grid.append('<div class="square" ' + 'data-coords="' + 
-          grid.gridCoords[i] + '">' + this.defaultSquare + '</div>');
+          this.gridCoords[i] + '">' + this.defaultSquare + '</div>');
     };
   },
   resetGrid: function() {
-    for(var i = 0; i < this.gridSize; i++) {
+    for(let i = 0; i < this.gridSize; i++) {
        $grid.find('[data-coords="' + this.gridCoords[i] + '"]')
           .html(this.defaultSquare).removeClass('is-snake');
     };
+  },
+  setCoordinates: function() {
+    let x = 0;
+    let y = 0;
+    for(let i = 0; i < this.gridSize; i++) {
+      if (i === 0 || i % this.xLength === 0) {
+        x++;
+      }
+      if (i <= 39) {
+        y = i+1;
+      } else {
+        y = (i % this.xLength)+1;
+      }
+      this.gridCoords[i] = [x, y];  
+    };
+    return this.gridCoords;
   }
 };
 
@@ -53,24 +50,22 @@ var snake = {
   },
   render: function() {
     // render head
-    var headSquare = $grid.find('[data-coords="' + this.headPosition + '"]');
+    let headSquare = $grid.find('[data-coords="' + this.headPosition + '"]');
     headSquare.html(this.head);
 
     // render body
-    var $body = this.snakeBody;
     for(var i = 1; i < this.snakeBody.length; i++) {
-      var memberSquare = $grid.find('[data-coords="' + this.snakeBody[i] + '"]');
+      let memberSquare = $grid.find('[data-coords="' + this.snakeBody[i] + '"]');
       memberSquare.addClass('is-snake');
     }
   }
 }
 
+// Run the game
 $(document).ready(function() {
-  // init grid
-  grid.gridCoords = getCoordinates(grid.gridSize, grid.xLength);
+  // initiate grid and snake
+  grid.setCoordinates();
   grid.renderGrid();
-
-  // render snake
   snake.render();
 
   // reset, move and re-render
